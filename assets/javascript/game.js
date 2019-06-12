f31f9d69cfe540e34449d2f9ede70b6f0172c88e
Saeed Altaf <saeedaltaf@gmail.com>
2019-06-12 17:26:25 -0400

    Added images to assets folder

---
+++ assets/javascript/game.js
@@ -0,0 +1,5 @@
+var randomWordArray = ["Acura", "Audi", "BMW", "Bentley", "Bugatti", "Buick", "Cadillac",
+    "Chevrolet", "Chrysler", "Citroen", "Dodge", "Ferrari", "Fiat", "Ford", "GMC", "Honda", "Hyundai",
+    "Infiniti", "Jaguar", "Jeep", "Kia", "Koenigsegg", "Lamborghini", "Land Rover", "Lexus", "Lincoln",
+    "Mazda", "Mclaren", "Maserati", "Mercedes", "Mini", "Mitsubishi", "Nissan", "Pagani", "Peugeot", "Porsche",
+    "Renault", "Rolls Royce", "Skoda", "Tesla", "Smart", "Subaru", "Suzuki", "Toyota", "Volkswagen", "Volvo"];
