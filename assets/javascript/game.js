8bdd5035ce2bbfd5e7da16393e57699a90c4d275
Saeed Altaf <saeedaltaf@gmail.com>
2019-06-14 16:42:56 -0400

    Added colours and updated pseudocode in javascript for clarity

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,49 +5,67 @@ var word = ["Acura", "Audi", "BMW", "Bentley", "Bugatti", "Buick", "Cadillac",
     "Mazda", "Mclaren", "Maserati", "Mercedes", "Mini", "Mitsubishi", "Nissan", "Pagani", "Peugeot", "Porsche",
     "Renault", "Rolls Royce", "Skoda", "Tesla", "Smart", "Subaru", "Suzuki", "Toyota", "Volkswagen", "Volvo"];
 
-//To pick a random word:
-var currentWord = word[Math.floor(Math.random() * word.length)];
+//To choose word randomly from word bank array:
+var randNum = Math.floor(Math.random() * word.length);
+var randomWord = word[randNum];
 
-//To set up the answer array by showing "_" for each index:
-var answerArray = [];
-for (i = 0; i < word.length; i++) {
-    answerArray[i] = "_";
-};
+var rightWord = [];
+var wrongWord = [];
+var underScore = [];
 
-//Variable to show how many letters are remaining to be guessed:
-var remainingLetters = word.length;
 
-//To keep score and guesses:
-var guessedLetters = "";
-var wins = 0;
-var guessesRemaining = 10;
 
-//Variables to write to html:
-var guessesRemainingText = document.getElementById("guessesremaining-text");
-var lettersGuessedText = document.getElementById("lettersguessed-text");
-var winsText = document.getElementById("wins-text");
-var gamePlayText = document.getElementById("gameplay-text");
-var directionsText = document.getElementById("directions");
 
-////////////////////////////////////
-// *****CODE FOR THE MAIN GAME*****
-////////////////////////////////////
 
-document.onkeyup = function (event) {
-    var userGuess = event.key;
-    // console.log("testing onkeyup function");
-    //Update guess into the game:
-    while (remainingLetters > 0) {
-        gamePlayText.textContent = (answerArray.join(" "));
-        for (var j = 0; j < word.length; j++){
-            if (word[j] === guess);
-            guessesRemaining--;
-            }  
+// //To pick a random word:
+// var currentWord = word[Math.floor(Math.random() * word.length)];
 
-        directionsText.textContent = "";
-        
-        }
-    }
+// //To have an array showing letters which were guessed:
+// var lettersGuessed = [];
+
+// //Variable to show how many letters are remaining to be guessed:
+// var remainingLetters = word.length;
+
+// //To keep score and guesses:
+// var guessedLetters = "";
+// var wins = 0;
+// var guessesRemaining = 10;
+
+// //Variables to write to html:
+// var guessesRemainingText = document.getElementById("guessesremaining-text");
+// var lettersGuessedText = document.getElementById("lettersguessed-text");
+// var winsText = document.getElementById("wins-text");
+// var gamePlayText = document.getElementById("gameplay-text");
+// var directionsText = document.getElementById("directions");
+
+// //To set up the answer array by showing "_" for each index:
+// var answerArray = [];
+// for (i = 0; i < word.length; i++) {
+//     answerArray[i] = "_";
+// };
+
+// ////////////////////////////////////
+// // *****CODE FOR THE MAIN GAME*****
+// ////////////////////////////////////
+
+// document.onkeyup = function (event) {
+//     var userGuess = event.key;
+//     // console.log("testing onkeyup function");
+//     //Update guess into the game:
+//     while (remainingLetters > 0) {
+//         gamePlayText.textContent = (answerArray.join(" "));
+//         for (var j = 0; j < word.length; j++){
+//             if (word[j] === userGuess);
+//             guessesRemaining--;
+//             } 
+
+//         directionsText.textContent = "";
+//         guessesRemainingText.textContent = "Guesses Remaining: " + guessesRemaining;
+//         winsText.textContent = "Wins: " + wins;
+
+
+//         }
+//     }
 
 
 
