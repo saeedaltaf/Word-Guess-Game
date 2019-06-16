a3c4f7aba0e2a89c5c296ec9dbf31d4b6aba4667
Saeed Altaf <saeedaltaf@gmail.com>
2019-06-15 23:14:30 -0400

    Created functions for specific tasks and corrected layout

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -26,7 +26,7 @@ var guessesRemainingText = document.getElementById("guessesremaining-text");
 var lettersGuessedText = document.getElementById("lettersguessed-text");
 
 //Create underscores based on length of random word which was chosen:
-var generateUnderscore = () => {
+function generateUnderscore() {
     for (var i = 0; i < randomWord.length; i++) {
         underScore.push("x");
     }
@@ -34,57 +34,85 @@ var generateUnderscore = () => {
 }
 
 //test to see if the underscore works:
-console.log(generateUnderscore());
+// console.log(generateUnderscore());
 //push underscores to game area
 gamePlayText.textContent = underScore;
 
-//Get user guess from key and if it's a capital letter convert to lowercase:
 document.onkeyup = function (event) {
-    var userGuess = (event.key).toLowerCase();
+    userGuess = (event.key).toLowerCase();
 
-    //Everytime a user presses a key, reduce guesses remaining by 1:
-    guessesRemaining--;
-    //If guesses remaining are zero, end game:    
-    console.log(underScore);
+    //Function to replace underscore with right letter  
+    function inputLetter() {
+        //For the length of the randomWord, we read each letter:
+        for (var x = 0; x < randomWord.length; x++) {
+            //If the userGuess is in the array randomWord:
+            if (userGuess === randomWord[x]) {
+                //Replace x with the correct letter guessed in the correct index within the word:
+                underScore[x] = userGuess;
+                gamePlayText.textContent = underScore.join("");
+            }
 
-
-    // if (guessesRemaining === 0){
-    //     alert("GAME OVER!");
-    // }
-    // if (randomWord === underScore){
-    //     wins++;
-    // }
-
-
-    
-
-    //For the length of the randomWord, we read each letter:
-    for (var x = 0; x < randomWord.length; x++) {
-        //If the userGuess is in the array randomWord:
-        if (userGuess === randomWord[x]) {
-            //Replace x with the correct letter guessed in the correct index within the word:
-            underScore[x] = userGuess;
-            gamePlayText.textContent = underScore.join("");
-        } else (userGuess != randomWord[x])
-            wrongLetter.push(userGuess);
-        
+        }
     }
-            
-        
-    //If the word is guessed (underscore)(join removes the commas), and matches the generated randomword, win!
-    if (underScore.join('') === randomWord) {
-        wins++;
-        alert("You won!");
-        guessesRemaining === 13;
-    //If guesses remaining equal 0, game over alert:
-    } else if (guessesRemaining === 0) {
-        alert("Game over!");
+
+    //Pushes incorrect guesses to lettersGuessed div in HTML and doesn't repeat incorrect guesses:
+    var wrongIndex = 0;
+    function wrongLettersGuessed() {
+        for (var i = 0; i < randomWord.length; i++) {
+            if (randomWord.indexOf(userGuess) === -1 && wrongLetter.indexOf(userGuess) === -1) {
+                wrongLetter[wrongIndex++] = userGuess;
+            }
+
+        }
 
     }
 
-    directionsText.textContent = "";
-    winsText.textContent = "Wins: " + wins;
-    guessesRemainingText.textContent = "Guesses Remaining: " + guessesRemaining;
-    lettersGuessedText.textContent = "Letters Guessed: " + wrongLetter;
+    // Resets the game to a new word:
+    function reset() {
+        guessesRemaining = 13;
+        randNum = Math.floor(Math.random() * word.length);
+        randomWord = word[randNum];
+        underScore = [];
+        wrongLetter = [];
+        wrongIndex = 0;
+        generateUnderscore();
 
+    }
+
+    //These functions will be called once the game resets (generating underscores for next word)
+    //and inputting user guesses in correct slots:
+    generateUnderscore();
+    inputLetter();
+
+
+    ///////////////////////////*********GAMEPLAY FUNCTIONS*******************/////////////////////
+
+    //Get user guess from key and if it's a capital letter convert to lowercase:
+    document.onkeyup = function (event) {
+        userGuess = (event.key).toLowerCase();
+        inputLetter();
+        wrongLettersGuessed();
+
+        //Everytime a user presses a key, reduce guesses remaining by 1:
+        guessesRemaining--;
+
+        //If the word is guessed (underscore)(join removes the commas), and matches the generated randomword, win!
+        if (underScore.join('') === randomWord) {
+            wins++;
+            alert("Congrats, you won!");
+            reset();
+            //If guesses remaining equal 0, game over alert:
+        }
+        if (guessesRemaining === 0) {
+            alert("Game Over!");
+            reset();
+
+        }
+
+        directionsText.textContent = "";
+        winsText.textContent = "Wins: " + wins;
+        guessesRemainingText.textContent = "Guesses Remaining: " + guessesRemaining;
+        lettersGuessedText.textContent = "Letters Guessed: " + wrongLetter;
+
+    }
 }
